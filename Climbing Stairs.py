0cc14b7d81f4f141575e547542c3ecd6d2aa898f
hecter <hecter654321@gmail.com>
2018-02-26 08:24:16 +0800

    pass testcase: testSample by using recursive

--- Climbing Stairs.py
+++ Climbing Stairs.py
@@ -5,11 +5,16 @@ class unitest(unittest.TestCase):
         Input = 0
         Output = 1
         self.assertEqual(Solution().climbStairs(Input),Output)
+    def testSample(self):
+        Input = 3
+        Output = 3
+        self.assertEqual(Solution().climbStairs(Input),Output)
 
 class Solution():
     def climbStairs(self, n):
-        if n == 0:
+        if n == 0 or n == 1:
             return 1
+        return self.climbStairs(n-1) + self.climbStairs (n-2)
 
 if __name__ == '__main__':
     unittest.main()
